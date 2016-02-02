0b4c1e1b8c7f04da3361782965ca210a0ee60202
Vince Montalbano <vince.montalbano@dev9.com>
2016-02-01 19:15:47 -0800

    step1

---
+++ src/main/java/com/dev9/example/springboot/BrownbagdemoApplication.java
@@ -0,0 +1,12 @@
+package com.dev9.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownbagdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrownbagdemoApplication.class, args);
+	}
+}
